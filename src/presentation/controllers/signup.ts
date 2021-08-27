c5b3fb287074e98f98fb1550dde8e4dea2582f9a
Edvaldo Santana <edypaulista152@gmail.com>
2021-08-27 16:02:44 -0300

    refactor: siplify code a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,12 @@ import { badRequest } from '../helpers/http-helper'
 export class SignUpController{
     
     handle(httpRequest: httpRequest): HttpResponse {
-        if (!httpRequest.body.name) {
-            return badRequest(new MissingParamError('name'))
-        }
+        const requiredFields = ['name', 'email']  
+        for (const field of requiredFields){
+            if(!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field))
+            }
 
-        if (!httpRequest.body.email) {
-            return badRequest(new MissingParamError('email'))
-        }       
+        }   
     }
 }
